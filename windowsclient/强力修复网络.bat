809e1d93a46281fc46c5a3e81b90b05d7d677d0f
rufengsuixing001 <1985151814@qq.com>
2017-04-10 17:14:09 +0800

    Add files via upload

---
+++ windowsclient/强力修复网络.bat
@@ -0,0 +1,6 @@
+Rd "%WinDir%\system32\test_permissions" >NUL 2>NUL
+Md "%WinDir%\System32\test_permissions" 2>NUL||(Echo 请使用右键管理员身份运行！&&Pause >nul&&Exit)
+Rd "%WinDir%\System32\test_permissions" 2>NUL
+NETSH INT IP RESET
+netsh winsock reset
+echo 请重启
