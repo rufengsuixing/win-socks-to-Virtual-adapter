e6fdc62878a9ef707cf7fa36d4d4b247f51a3903
rufengsuixing001 <1985151814@qq.com>
2017-04-10 20:13:40 +0800

    Update 轻度修复网络.bat

--- windowsclient/轻度修复网络.bat
+++ windowsclient/轻度修复网络.bat
@@ -3,17 +3,18 @@ Rd "%WinDir%\system32\test_permissions" >NUL 2>NUL
 Md "%WinDir%\System32\test_permissions" 2>NUL||(Echo 请使用右键管理员身份运行！&&Pause >nul&&Exit)
 Rd "%WinDir%\System32\test_permissions" 2>NUL
 NETSH WINHTTP RESET PROXY
-systeminfo>%~dp0tmpall.txt
-for /f "tokens=2 delims=:" %%a in ('findstr /r "10\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*" %~dp0tmpall.txt') do (set ip=%%a)
+cd "%~dp0"
+systeminfo>tmpall.txt
+for /f "tokens=2 delims=:" %%a in ('findstr /r "10\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*" tmpall.txt') do (set ip=%%a)
 if not defined ip echo 请连接校园网后开始或者使用强力修复 & pause & exit
 ::获取gate（没考虑掩码）
 for /f "tokens=1,2 delims=. " %%a in ('echo %ip%') do (set gate=%%a.%%b.0.1)
 ::获取主适配器名称
-for /f "tokens=1 delims=:" %%a in ('findstr /n /r "10\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*" %~dp0tmpall.txt') do (set wei2=%%a)
+for /f "tokens=1 delims=:" %%a in ('findstr /n /r "10\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*" tmpall.txt') do (set wei2=%%a)
 if not defined wei2 goto du
 set /a wei2=%wei2% - 5
 set "mainnamet="
-for /f "skip=%wei2% tokens=2* delims=:" %%a in (%~dp0tmpall.txt) do (if not defined mainnamet set "mainnamet=%%a")
+for /f "skip=%wei2% tokens=2* delims=:" %%a in (tmpall.txt) do (if not defined mainnamet set "mainnamet=%%a")
 for /f "tokens=* delims= " %%a in ("%mainnamet%") do call :ie "%%a"
 set mainname="%var%"
 netsh interface ip set interface %mainname% ignoredefaultroutes=disabled
