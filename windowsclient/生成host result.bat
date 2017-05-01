c6ac0f70711eaae6b276fd43839f8e16ff8b6bb9
rufengsuixing001 <1985151814@qq.com>
2017-05-01 15:15:11 +0800

    Delete 生成host result.bat

--- windowsclient/生成host result.bat
+++
@@ -1,8 +0,0 @@
-
-cd "%~dp0"
-for /f tokens^=3^ delims^=^/^" %%a in ('findstr "http://" source.txt') do (call :x1 %%a)
-
-:x1
-set "ip="
-for /f "tokens=2 delims=: " %%a in ('nslookup %~1 10.0.0.11 ^| findstr "Address:"') do (if not %%a==10.0.0.11 set ip=%%a)
-if "%ip:~0,3%"=="10." echo %ip%  %~1>>result.txt
