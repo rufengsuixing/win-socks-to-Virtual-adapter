42e28f8b22aa89c6bf8b4d09bd160cd69f875529
rufengsixing001 <1985151814@qq.com>
2017-05-07 13:11:30 +0800

    fix bug

--- windowsclient/还原hosts.bat
+++ windowsclient/还原hosts.bat
@@ -4,6 +4,7 @@ Rd "%WinDir%\System32\test_permissions" 2>NUL
 cd C:\Windows\System32\drivers\etc\
 if not exist hosts.bak (echo "没有备份过" & pause &exit)
 copy /y hosts.bak hosts
+del hosts.bak
 echo 还原完成
 pause
 exit
