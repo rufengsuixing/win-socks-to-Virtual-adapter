c6a2ba28e793b80607070a5e9a51c5aba2f9c4b3
rufengsuixing001 <1985151814@qq.com>
2017-05-27 22:48:29 +0800

    fix bug

--- windowsclient/修改host使用内网网站.bat
+++ windowsclient/修改host使用内网网站.bat
@@ -2,13 +2,13 @@
 Rd "%WinDir%\system32\test_permissions" >NUL 2>NUL
 Md "%WinDir%\System32\test_permissions" 2>NUL||(goto GetUAC)
 Rd "%WinDir%\System32\test_permissions" 2>NUL
-cd /d C:\Windows\System32\drivers\etc\
+cd /d %WinDir%\System32\drivers\etc\
 if exist hosts.bak (echo "曾经应用过，请点开还原后重试" & pause &exit)
 copy hosts hosts.bak
 echo 备份完成为hosts.bak
-cd "%~dp0"
+cd /d "%~dp0"
 if not exist result.txt (echo 请先生成result & pause & exit)
-for /f "tokens=1,2 delims= " %%a in (result.txt) do (echo %%a %%b>>C:\Windows\System32\drivers\etc\hosts)
+for /f "tokens=1,2 delims= " %%a in (result.txt) do (echo %%a %%b>>%WinDir%\System32\drivers\etc\hosts)
 ipconfig /flushdns
 echo 修改完成
 pause
